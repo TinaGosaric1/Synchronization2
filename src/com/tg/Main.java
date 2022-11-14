7d5310a5fa62d9f2aa2e228e3c223b3f03c4b5c9
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-14 13:59:12 +0100

    Fixing gitignore file

---
+++ src/com/tg/Main.java
@@ -0,0 +1,7 @@
+package com.tg;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/tg/ThreadColor.java
@@ -0,0 +1,4 @@
+package com.tg;
+
+public class ThreadColor {
+}
